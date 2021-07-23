e3d0c5e175815b5890a61e30485e28e96157cff1
endezz <r-serega13@yandex.ru>
2021-07-23 14:33:12 +0400

    feat: add open modal window script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,39 @@
 window.onload = function () {
+  // Modal
+  const buttonSignIn = document.getElementById('btn_sign-in');
+  const buttonOpenSignIn = document.getElementById('btn_open-sign-in');
+  const modal = document.getElementById('modal');
+  const overlay = document.getElementById('overlay');
+  const body = document.querySelector('body');
+
+  const showModal = (e) => {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+  };
+
+  const hideModal = () => {
+    modal.classList.add('hidden');
+  };
+
+  const showOverlay = (e) => {
+    e.preventDefault();
+    overlay.classList.remove('hidden');
+    body.style.overflow = 'hidden';
+    showModal(e);
+  };
+
+  const hideOverlay = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    overlay.classList.add('hidden');
+    body.style.overflow = 'visible';
+    hideModal(e);
+  };
+
+  buttonOpenSignIn.addEventListener('click', showOverlay);
+  overlay.addEventListener('click', hideOverlay);
+
+  // Tabs toggle
   const buttonMovies = document.querySelector('.button__movies');
   const buttonTV = document.querySelector('.button__tv');
   const tabsContent = document.querySelectorAll('.tabs__content');
